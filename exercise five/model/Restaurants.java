f99354f917a1dfa44b8e9d7ba2a58f5fe2f5331e
Mikaella Mendoza <122050772+444zip@users.noreply.github.com>
2023-05-26 21:21:04 +0800

    Create Restaurants.java

---
+++ exercise five/model/Restaurants.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurants {
+
+    private String bestseller;
+    private String rate;
+
+    public String getBestSeller() {
+        return this.bestseller;
+    }
+
+    public void setBestSeller(String bestseller) {
+        this.bestseller = bestseller;
+    }
+
+    public String getRate() {
+        return this.rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+}
